dda787fd183521c10cb14e1b8be795c274d2a978
Михаил <mihail@MacBook-Pro.local>
2022-10-23 17:17:16 +0400

    rename functions

--- WhiteAppTy/ViewController.swift
+++ WhiteAppTy/ViewController.swift
@@ -11,17 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print ("Мой текущий возраст составляет \(MyAge)")
-        print ("Мой текущий вес составляет \(MyWeight)")
-        print ("У квадрата всегда \(Nsos) стороны")
+        print ("Мой текущий возраст составляет \(myage)")
+        print ("Мой текущий вес составляет \(myweight)")
+        print ("У квадрата всегда \(nsos) стороны")
         print (me)
         
         // Do any additional setup after loading the view.
     }
-    var MyAge : Int = 33, MyWeight : Float = 75.05
-    let MyMame : String = "Mike"
-    let Nsos : Int = 4 // number of sides of a square
-    lazy var me = ("Мне \(MyAge), и мой вес сейчас \(MyWeight), а еще я знаю что у квадрата \(Nsos) стороны")
+    var myage : Int = 33, myweight : Float = 75.05
+    let myname : String = "Mike"
+    let nsos : Int = 4 // number of sides of a square
+    lazy var me = ("Мне \(myage), и мой вес сейчас \(myweight), а еще я знаю что у квадрата \(nsos) стороны")
     
 
 }
